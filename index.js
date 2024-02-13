277e21d12e8b4dbe27c94bc4f02e4bfaf66169e0
aArntzen <158445429+aArntzen@users.noreply.github.com>
2024-02-13 13:52:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function johan(){
-    let johan = ginger
+let display = document.querySelector('#timer');
+let seconds = 0;
+let timerInterval;
+
+function startTimer() {
+    timerInterval = setInterval(function () {
+        let minutes = parseInt(seconds / 60, 10);
+        let remainingSeconds = parseInt(seconds % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        remainingSeconds = remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
+
+        display.textContent = minutes + ":" + remainingSeconds;
+
+        seconds++;
+    }, 1000);
 }
