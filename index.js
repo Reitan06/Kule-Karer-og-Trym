2a0ec85704fcd8d6c4b708c29a70e7230e98f1a0
Mads-Dahl <158445515+Mads-Dahl@users.noreply.github.com>
2024-02-29 09:59:12 +0100

    format

--- index.js
+++ index.js
@@ -21,23 +21,13 @@ function startTimer() {
         display.textContent = formattedMinutes + ":" + formattedSeconds;
     }, 1000);
     document.getElementById('startknapp').style.display = 'none'
-    
-    setInterval(function() {
-        ataler+=2;
+
+    setInterval(function () {
+        ataler += 2;
         console.log("Antall tellere: " + ataler);
         document.querySelector('.penger p').textContent = "Antall Ataler: " + ataler + "α";
     }, 1000);
 
-}
-function startSound() {
-    var themesong = document.getElementById("themesong");
-
-    // Check if the audio is paused or not
-    if (themesong.paused) {
-        themesong.play(); // If paused, play the audio
-    } else {
-        themesong.pause(); // If playing, pause the audio
-    }
 }
 
 function moveCharacterRight(characterElement) {
@@ -54,78 +44,77 @@ function moveCharacterRight(characterElement) {
 function summonTrym() {
     const trym = document.createElement("img");
     trym.src = "bilder/WalkingTrym.png";
-    trym.alt = "trym"; 
+    trym.alt = "trym";
     trym.classList.add("trym");
-    trym.style.left = "80px"; 
-    trym.style.top = "400px"; 
+    trym.style.left = "80px";
+    trym.style.top = "400px";
     document.body.appendChild(trym);
-    ataler -=100
+    ataler -= 100
 }
 
-    function summonJohan() {
-        const johan = document.createElement("img");
-        johan.src = "bilder/johan_walking.png";
-        johan.alt = "Johan";
-        johan.classList.add("Johan");
-        johan.style.left = "80px";
-        johan.style.top = "600px";
-        document.body.appendChild(johan);
-        moveCharacterRight(johan); // Start moving Johan right after summoning
-        ataler -=50
+function summonJohan() {
+    const johan = document.createElement("img");
+    johan.src = "bilder/johan_walking.png";
+    johan.alt = "Johan";
+    johan.classList.add("Johan");
+    johan.style.left = "80px";
+    johan.style.top = "600px";
+    document.body.appendChild(johan);
+    moveCharacterRight(johan); // Start moving Johan right after summoning
+    ataler -= 50
+}
+
+function summonPer() {
+    const per = document.createElement("img");
+    per.src = "bilder/MovingPer.png";
+    per.alt = "Per";
+    per.classList.add("per");
+    per.style.left = "80px";
+    per.style.top = "600px";
+    document.body.appendChild(per);
+    moveCharacterRight(per); // Start moving Per right after summoning
+    ataler -= 20
+}
+
+function summonJokkis() {
+    const jokkis = document.createElement("img");
+    jokkis.src = "bilder/MovingJokkis.png";
+    jokkis.alt = "Jokkis";
+    jokkis.classList.add("jokkis");
+    jokkis.style.left = "80px";
+    jokkis.style.top = "600px";
+    document.body.appendChild(jokkis);
+    moveCharacterRight(jokkis); // Start moving Jokkis right after summoning
+    ataler -= 10
+}
+
+function startSound() {
+    var themesong = document.getElementById("themesong");
+
+    // Check if the audio is paused or not
+    if (themesong.paused) {
+        themesong.play(); // If paused, play the audio
+    } else {
+        themesong.pause(); // If playing, pause the audio
     }
+}
 
-    function summonPer() {
-        const per = document.createElement("img");
-        per.src = "bilder/MovingPer.png";
-        per.alt = "Per";
-        per.classList.add("per");
-        per.style.left = "80px";
-        per.style.top = "600px";
-        document.body.appendChild(per);
-        moveCharacterRight(per); // Start moving Per right after summoning
-        ataler -=20
-    }
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var img = new Image();
+img.src = "bilder/johan_walking.png";
+var x = 0;
+var y = 220;
 
-    function summonJokkis() {
-        const jokkis = document.createElement("img");
-        jokkis.src = "bilder/MovingJokkis.png";
-        jokkis.alt = "Jokkis";
-        jokkis.classList.add("jokkis");
-        jokkis.style.left = "80px";
-        jokkis.style.top = "600px";
-        document.body.appendChild(jokkis);
-        moveCharacterRight(jokkis); // Start moving Jokkis right after summoning
-        ataler -=10
-    }
-    function startSound() {
-        var themesong = document.getElementById("themesong");
-    
-        // Check if the audio is paused or not
-        if (themesong.paused) {
-            themesong.play(); // If paused, play the audio
-        } else {
-            themesong.pause(); // If playing, pause the audio
-        }
-    }
+function moveImage() {
 
-    var canvas = document.getElementById('myCanvas');
-    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    x += 1;
 
-    var img = new Image();
-    img.src = "bilder/johan_walking.png"; 
-    var x = 0;
-    var y = 220;
-
-    function moveImage() {
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        x += 1;
-
-        ctx.drawImage(img, x, y, 184, 184);
-        requestAnimationFrame(moveImage);
-    }
-    img.onload = function() {
-        moveImage();
-    };
+    ctx.drawImage(img, x, y, 184, 184);
+    requestAnimationFrame(moveImage);
+}
+img.onload = function () {
+    moveImage();
+};
